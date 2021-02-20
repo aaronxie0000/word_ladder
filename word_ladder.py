6349d02c6ca0678b729c426a10f47e8fda2e6b48
Aaron Xie <aaron.xie2024@gmail.com>
2021-02-20 01:55:35 -0800

    fix lint error

--- word_ladder.py
+++ word_ladder.py
@@ -74,9 +74,9 @@ def verify_word_ladder(ladder):
     '''
     if (len(ladder) == 0):
         return False
-    for i in range(0, len(ladder)-1):
-        if (not _adjacent(ladder[i], ladder[i+1])):
-                return False
+    for i in range(0, len(ladder) - 1):
+        if (not _adjacent(ladder[i], ladder[i + 1])):
+            return False
     return True
 
 
